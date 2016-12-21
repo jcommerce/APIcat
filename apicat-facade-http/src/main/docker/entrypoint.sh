a1114971d26bea0b4e6b3e083187fc00d827c751
Dawid Janaszak <dawid.janaszak@jcommerce.pl>
2016-12-21 15:00:26 +0100

    Restored project structure and removed unnecessary code

--- apicat-facade-http/src/main/docker/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dockerize -wait tcp://apicat-mysql:3306 -timeout 30s
-
-java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=docker -jar /app.jar
--- apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/model/model/ApiFormat.java
+++
@@ -1,8 +0,0 @@
-package pl.jcommerce.apicat.model.model;
-
-/**
- * Created by jada on 13.12.2016.
- */
-public enum ApiFormat {
-    SWAGGER
-}
