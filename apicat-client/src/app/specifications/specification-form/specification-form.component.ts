90f41d4b1a7f0d3f97f79edd323f54b0eddec6ab
Dawid Janaszak <dawid.janaszak@jcommerce.pl>
2016-12-28 13:47:42 +0100

    Added specification form validation

--- apicat-client/src/app/specifications/specification-form/specification-form.component.ts
+++ apicat-client/src/app/specifications/specification-form/specification-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from "@angular/core";
 import {ApiFormatService} from "../../common/api-format.service";
 import {Observable} from "rxjs";
 import {Specification} from "../../model/specification";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'specification-form',
@@ -15,8 +16,17 @@ export class SpecificationFormComponent implements OnInit {
   @Output('submitSpecification') submitEmitter = new EventEmitter<Specification>();
 
   formats: Observable<string[]>;
+  specificationForm: FormGroup;
 
-  constructor(private apiFormatService: ApiFormatService) {
+  constructor(private apiFormatService: ApiFormatService,
+              fb: FormBuilder) {
+    this.specificationForm = fb.group({
+      'name': [null, Validators.required],
+      'author': [null, Validators.required],
+      'version': [null, Validators.required],
+      'format': [null, Validators.required],
+      'content': [null, Validators.required]
+    })
   }
 
   ngOnInit(): void {
