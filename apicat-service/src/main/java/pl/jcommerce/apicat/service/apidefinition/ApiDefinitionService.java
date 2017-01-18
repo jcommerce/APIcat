62f9ac84559bd57fb8bb56d60566e611866fa366
LUWA <lukasz.wasiak@jcommerce.pl>
2017-01-18 10:29:35 +0100

    ApiDefinitionService fix

--- apicat-service/src/main/java/pl/jcommerce/apicat/service/apidefinition/ApiDefinitionService.java
+++ apicat-service/src/main/java/pl/jcommerce/apicat/service/apidefinition/ApiDefinitionService.java
@@ -1,9 +1,9 @@
 package pl.jcommerce.apicat.service.apidefinition;
 
-import pl.jcommerce.apicat.contract.ApiDefinition;
 import pl.jcommerce.apicat.contract.validation.result.ValidationResult;
 import pl.jcommerce.apicat.service.apidefinition.dto.ApiDefinitionCreateDto;
 import pl.jcommerce.apicat.service.apidefinition.dto.ApiDefinitionUpdateDto;
+import pl.jcommerce.apicat.service.apidefinition.dto.ApiDefinitionDto;
 
 import java.util.List;
 
@@ -14,7 +14,7 @@ public interface ApiDefinitionService {
 
     Long createDefinition(ApiDefinitionCreateDto data);
 
-    ApiDefinition getDefinition(Long id);
+    ApiDefinitionDto getDefinition(Long id);
 
     Long updateDefinition(ApiDefinitionUpdateDto data);
 
---
+++ apicat-service/src/main/java/pl/jcommerce/apicat/service/apidefinition/dto/ApiDefinitionDto.java
@@ -0,0 +1,7 @@
+package pl.jcommerce.apicat.service.apidefinition.dto;
+
+/**
+ * Created by luwa on 18.01.17.
+ */
+public class ApiDefinitionDto {
+}
