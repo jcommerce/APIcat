a77e2b6ea3a81d9cde9ca285a22472b304bd88a4
pholak <przemyslaw.holak@jcommerce.pl>
2017-01-18 10:53:43 +0100

    Add @Repository annotation to dao.

--- apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/dao/impl/ApiDefinitionDaoImpl.java
+++ apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/dao/impl/ApiDefinitionDaoImpl.java
@@ -1,10 +1,12 @@
 package pl.jcommerce.apicat.dao.impl;
 
+import org.springframework.stereotype.Repository;
 import pl.jcommerce.apicat.dao.ApiDefinitionDao;
 
 /**
  * Created by prho on 17.01.17.
  */
 
+@Repository
 public class ApiDefinitionDaoImpl extends GenericDaoImpl implements ApiDefinitionDao {
 }
--- apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/dao/impl/ApiSpecificationDaoImpl.java
+++ apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/dao/impl/ApiSpecificationDaoImpl.java
@@ -1,10 +1,12 @@
 package pl.jcommerce.apicat.dao.impl;
 
+import org.springframework.stereotype.Repository;
 import pl.jcommerce.apicat.dao.ApiSpecificationDao;
 
 /**
  * Created by prho on 17.01.17.
  */
 
+@Repository
 public class ApiSpecificationDaoImpl extends GenericDaoImpl implements ApiSpecificationDao {
 }
