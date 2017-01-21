d8419b69f6e7aa8082aca11ec13fbe5c0f574838
Pawel Witkowski <pawel.witkowski@jcommerce.pl>
2017-01-21 12:47:51 +0100

    add MappedSuperclass

--- apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/model/AbstractBaseModel.java
+++ apicat-repository-jpa/src/main/java/pl/jcommerce/apicat/model/AbstractBaseModel.java
@@ -6,21 +6,20 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.Date;
 
-/**
- * Created by prho on 17.01.17.
- */
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class AbstractBaseModel {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     Long id;
 
-    @Column(name = "CREATION_DATE")
+    @Column(name = "creation_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date creationDate;
 
-    @Column(name = "MODIFICATION_DATE")
+    @Column(name = "modification_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date modificationDate;
 
